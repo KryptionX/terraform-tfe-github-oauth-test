4c20809c302bd093bee16dc47ee45549c4a17560
Johnny G <encryptedkey@icloud.com>
2022-09-02 16:46:59 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,25 @@
 variable "hostname" {
   type = string
-  # default     = "app.terraform.io"
-  description = "FQDN of the host in question. Defaults to 'app.terraform.io'"
+  description = "FQDN of the host in question. Example 'app.terraform.io' or 'shame-wizard.tf-support.hashicorpdemo.com."
 }
 variable "client_name" {
   type = string
-  # default     = "GitHub (Your_Username)"
-  description = "Name of the GitHub client connection saved in the Providers tab." # Recommend "GitHub (Your_Username)" for easy identification
+  description = "Name of the GitHub client connection saved in the Providers tab. Using "GitHub (Your_Username)" makes for easy identification in shared org environments."
 }
 
 variable "token" {
   type        = string
-  description = "User token with Owners team permissions"
+  description = "User token with Owners team permissions."
   sensitive   = true
 }
 
 variable "org" {
   type        = string
-  description = "The name of the TFE/C Org you are wanting to attach this GitHub OAuth connection to."
+  description = "The name of the TFC/E Org you are wanting to attach this GitHub VCS connection to."
 }
 
 variable "github_pat" {
   type        = string
-  description = "Your GitHub user Personal Access Token. Can be obtained via https://github.com/settings/tokens"
+  description = "Your GitHub user Personal Access Token. Can be obtained using https://github.com/settings/tokens"
   sensitive   = true
 }
