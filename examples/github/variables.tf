b48e7b64087116651a1235bb16b9870403292294
KryptionX <johnny@hashicorp.com>
2022-09-02 15:50:03 -0600

    added sensitive flag

--- examples/github/variables.tf
+++ examples/github/variables.tf
@@ -1,5 +1,27 @@
-variable "hostname" {}
-variable "client_name" {}
-variable "token" {}
-variable "org" {}
-variable "github_pat" {}
+variable "hostname" {
+  type        = string
+  default     = "app.terraform.io"
+  description = "FQDN of the host in question. Defaults to 'app.terraform.io'"
+}
+variable "client_name" {
+  type        = string
+  default     = "GitHub (Your_Username)"
+  description = "Name of the GitHub client connection saved in the Providers tab." # Recommend "GitHub (Your_Username)" for easy identification
+}
+
+variable "token" {
+  type        = string
+  description = "User token with Owners team permissions"
+  sensitive   = true
+}
+
+variable "org" {
+  type        = string
+  description = "The name of the TFE/C Org you are wanting to attach this GitHub OAuth connection to."
+}
+
+variable "github_pat" {
+  type        = string
+  description = "Your GitHub user Personal Access Token. Can be obtained via https://github.com/settings/tokens"
+  sensitive   = true
+}
