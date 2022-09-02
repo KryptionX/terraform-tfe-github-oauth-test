c211d540dafe632238259d5cabbd285a4677c1ba
KryptionX <johnny@hashicorp.com>
2022-09-02 16:24:24 -0600

    changed module var from name to client_name

--- examples/github/main.tf
+++ examples/github/main.tf
@@ -1,10 +1,10 @@
 module "github_oauth" {
   source  = "app.terraform.io/kryptionx/github-oauth/tfe"
-  version = "0.1.4"
+  version = "0.1.7"
 
-  hostname   = var.hostname
-  name       = var.client_name
-  token      = var.token
-  org        = var.org
-  github_pat = var.github_pat
+  hostname    = var.hostname
+  client_name = var.client_name
+  token       = var.token
+  org         = var.org
+  github_pat  = var.github_pat
 }
