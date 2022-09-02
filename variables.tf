57ee34ba9041e20e6df739e66c9947b9676fc521
Johnny G <encryptedkey@icloud.com>
2022-09-02 14:05:11 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "hostname" {
+  type        = string
+  description = "FQDN of the host in question. eg. 'app.terraform.io'"
+}
+
+variable "token" {
+  type        = string
+  description = "User token with Owners team permissions"
+}
+
+variable "github_pat" {
+  type        = string
+  description = "Your GitHub user Personal Access Token. Can be obtained via https://github.com/settings/tokens"
+}
+
+variable "org" {
+  type        = string
+  description = "The name of the TFE/C Org you are wanting to attach this GitHub OAuth connection to."
+}
